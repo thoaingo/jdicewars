86cb4736084758f115f6dc0866a93896747ffbec
Tkachuk.I.M <Tkachuk.I.M@af14a0ee-5fef-11de-bd89-139d19cb17f4>
2009-08-14 11:39:10 +0000

    added MessaeBundle

--- trunk/client/src/app/StartApp.java
+++ trunk/client/src/app/StartApp.java
@@ -6,17 +6,22 @@ import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
+import util.MessageBundle;
+
 public class StartApp {
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+					// UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+					UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
 				} catch (Exception e) {
 					// TODO
 				}
-			
+				
+				MessageBundle.initLocale("en");
+				
 				JFrame mainFraime = new MainFrame();
 				mainFraime.setVisible(true);
 			}
--- trunk/client/src/gui/frame/MainFrame.java
+++ trunk/client/src/gui/frame/MainFrame.java
@@ -4,19 +4,23 @@ import gui.ContainerPanel;
 import gui.menu.MainMenuBar;
 
 import javax.swing.JFrame;
+import javax.swing.JMenuBar;
 import javax.swing.JPanel;
+
+import util.MessageBundle;
 
 @SuppressWarnings("serial")
 public class MainFrame extends JFrame {
 
 	public MainFrame() {
-		super("JDiceWars");
+		super(MessageBundle.get("mainframe.title"));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         JPanel containerPanel = new ContainerPanel();
+        JMenuBar mainMenu = new MainMenuBar(containerPanel);
         
         getContentPane().add(containerPanel);
-        setJMenuBar(new MainMenuBar(containerPanel));
+        setJMenuBar(mainMenu);
         
         setSize(640, 480);
         setLocationRelativeTo(null);	
